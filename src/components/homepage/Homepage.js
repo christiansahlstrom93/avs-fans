4d1ab0d9fb6086aea438161cef196bb016b399b2
Christian Sahlström <christiansahlstrom@Christians-MacBook-Pro.local>
2021-10-16 07:54:43 +0200

    Started with point leader board

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,6 +1,5 @@
 import { useCallback, useState } from 'react';
 import LeageuStats from '../leagueStats/LeagueStats';
-import TeamStats from '../leagueStats/TeamStats';
 import Players from '../players/Players';
 import Tab from '../tab/Tab';
 import banner from './banner.jpeg';
@@ -8,7 +7,6 @@ import './Homepage.css';
 
 const PLAYERS_TAB = 0;
 const STATS_TAB = 1;
-const TEAM_STATS_TAB = 2;
 
 const Homepage = () => {
   const [ activeTab, setActiveTab ] = useState(0);
@@ -17,8 +15,6 @@ const Homepage = () => {
       return <Players />;
     } else if (activeTab === STATS_TAB) {
       return <LeageuStats />
-    } else if (activeTab === TEAM_STATS_TAB) {
-      return <TeamStats />
     }
     return null;
   }, [activeTab]);
@@ -31,7 +27,6 @@ const Homepage = () => {
       <div className="tabs">
         <Tab isActive={activeTab === PLAYERS_TAB} name="Roster" id={PLAYERS_TAB} onClick={onTabClick} />
         <Tab isActive={activeTab === STATS_TAB} name="Stats" id={STATS_TAB} onClick={onTabClick} />
-        <Tab isActive={activeTab === TEAM_STATS_TAB} name="Team stats" id={TEAM_STATS_TAB} onClick={onTabClick} />
       </div>
       {getContent()}
     </div>
