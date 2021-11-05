d95ee6edb589eefe3b4f231dab06771daba19982
Christian Sahlström <christiansahlstrom@Christians-MacBook-Pro.local>
2021-11-05 11:59:34 +0100

    Use cool NPM package

--- src/App.js
+++ src/App.js
@@ -6,13 +6,19 @@ import PlayerDetails from './components/players/PlayerDetails';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import { index,playerDetails } from './routes';
-import Schedule from './components/schedule/Schedule';
 import { ScheduleContextProvider } from './contexts/ScheduleContext';
 import { LeagueStatsContextProvider } from './contexts/LeagueStatsContext';
+import { WithScheduleWrapper } from '@christiansahlstrom/nhl-cool-widget';
 import './App.css';
 import { PointSummaryContextProvider } from './contexts/PointSummaryContext';
+import ScheduleHeader from './components/schedule/ScheduleHeader';
+import { useCallback, useState } from 'react';
 
 const App = () => {
+  const [ shouldFilter, setFilter ] = useState(false);
+  const onFilter = useCallback(() => {
+    setFilter(!shouldFilter)
+  }, [shouldFilter, setFilter]);
   return (
     <PlayersContextProvider>
       <PointSummaryContextProvider>
@@ -20,7 +26,10 @@ const App = () => {
           <ScheduleContextProvider>
             <LeagueStatsContextProvider>
               <Header />
-              <Schedule />
+              <>
+                <ScheduleHeader onFilter={onFilter} isActive={shouldFilter} />
+                <WithScheduleWrapper filterActive={shouldFilter} teamId={21} />
+              </>
               <BrowserRouter basename="/">
                 <Switch>
                   <Route exact path={index} component={Homepage} />
