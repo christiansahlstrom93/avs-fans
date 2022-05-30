d95ddfc1303f479a243f7026b2a52e6daef5bb32
Christian Sahlström <christian.frahm.sahlstrom@ingka.ikea.com>
2022-05-30 12:20:10 +0200

    Fix stuff

--- src/config.js
+++ src/config.js
@@ -11,7 +11,7 @@ const Config = {
       logo: 'https://www-league.nhlstatic.com/images/logos/teams-current-primary-light/21.svg',
       color: '#98012e',
     },
-    banner: 'https://wallpapercave.com/wp/wp1964095.png',
+    banner: 'https://www.pixelstalk.net/wp-content/uploads/2016/10/Colorado-Avalanche-Backgrounds-Free-Download.jpg',
     scheduleFilter: {},
     footer: {
       logo: 'https://www-league.nhlstatic.com/images/logos/league-dark/133-flat.svg',
