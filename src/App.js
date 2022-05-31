0a7d5c83c026bc3210adeeccb6a35f3201cf44cb
Christian Sahlström <christian.frahm.sahlstrom@ingka.ikea.com>
2022-05-31 10:56:42 +0200

    new stuff

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
-import {PlayersContextProvider} from './contexts/PlayersContext'
-import { PlayerDetailsContextProvider } from './contexts/PlayerDetailsContext';
-import Homepage from './components/homepage/Homepage'
-import PlayerDetails from './components/players/PlayerDetails';
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
-import { index,playerDetails } from './routes';
-import { ScheduleContextProvider } from './contexts/ScheduleContext';
-import { LeagueStatsContextProvider } from './contexts/LeagueStatsContext';
-import './App.css';
-import { PointSummaryContextProvider } from './contexts/PointSummaryContext';
-import Schedule from './components/schedule/Schedule'
-import { ConfigContextProvider } from './contexts/ConfigContext';
-import { SimulationContextProvider } from './contexts/SimulationContext';
-import Simulation from './components/schedule/Simulation';
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+import { PlayersContextProvider } from "./contexts/PlayersContext";
+import { PlayerDetailsContextProvider } from "./contexts/PlayerDetailsContext";
+import Homepage from "./components/homepage/Homepage";
+import PlayerDetails from "./components/players/PlayerDetails";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import { index, playerDetails } from "./routes";
+import { ScheduleContextProvider } from "./contexts/ScheduleContext";
+import { LeagueStatsContextProvider } from "./contexts/LeagueStatsContext";
+import "./App.css";
+import { PointSummaryContextProvider } from "./contexts/PointSummaryContext";
+import Schedule from "./components/schedule/Schedule";
+import { ConfigContextProvider } from "./contexts/ConfigContext";
+import { SimulationContextProvider } from "./contexts/SimulationContext";
+import Simulation from "./components/schedule/Simulation";
+import "./App.css";
 
 const App = () => {
+  const isMobile = () => window.innerWidth <= 800;
+
+  if (!isMobile()) {
+    return (
+      <div className="warning">
+        <img className="phoneImg" src="/logos/iphone.png" />
+        <span className="phoneText">
+          This application is for mobile devices only
+        </span>
+      </div>
+    );
+  }
+
   return (
     <SimulationContextProvider>
       <ConfigContextProvider>
@@ -30,7 +44,11 @@ const App = () => {
                   <BrowserRouter basename="/">
                     <Switch>
                       <Route exact path={index} component={Homepage} />
-                      <Route exact path={playerDetails} component={PlayerDetails} />
+                      <Route
+                        exact
+                        path={playerDetails}
+                        component={PlayerDetails}
+                      />
                     </Switch>
                   </BrowserRouter>
                   <Footer />
@@ -42,6 +60,6 @@ const App = () => {
       </ConfigContextProvider>
     </SimulationContextProvider>
   );
-}
+};
 
 export default App;
--- src/components/schedule/Simulation.js
+++ src/components/schedule/Simulation.js
@@ -1,52 +1,79 @@
-import React, { useContext } from 'react';
-import { SimulationContext } from '../../contexts/SimulationContext';
-import './Simulation.css';
+import React, { useContext } from "react";
+import { SimulationContext } from "../../contexts/SimulationContext";
+import "./Simulation.css";
 
 const Simulation = () => {
-  const [ { data, loading } ] = useContext(SimulationContext);
+  const [{ data, loading }, , clear] = useContext(SimulationContext);
+
+  const onCloseClick = () => {
+    clear();
+  };
+
+  const renderTopBar = () => (
+    <div className="closeBar">
+      <img className="closeImg" src="/logos/close.png" onClick={onCloseClick} />
+    </div>
+  );
 
   if (loading) {
     return (
-      <div className="simulation">
-        <div className="loading">
-          Loading...
+      <div>
+        {renderTopBar()}
+        <div className="loadingSim">
+          <div className="loader circle" />
+          <div className="loadingText">Simulating...</div>
         </div>
       </div>
-    )
+    );
   }
 
   if (!data) {
     return null;
   }
 
-  if (!data[0].totalAwayGames || !data[0].totalHomeGames || !data[1].totalAwayGames || !data[1].totalHomeGames) {
+  if (
+    !data[0].totalAwayGames ||
+    !data[0].totalHomeGames ||
+    !data[1].totalAwayGames ||
+    !data[1].totalHomeGames
+  ) {
     return (
       <div className="simulation">
-        <div className="loading">
+        {renderTopBar()}
+        <div className="no-games">
           Not enough games played to make simulations
         </div>
       </div>
-    )
+    );
   }
 
   const renderTeamData = (simData, isHome) => (
     <div className="simTeam">
       <div className="simTeamNameContainer">
         <div className="simTeamName">{simData.teamData.name}</div>
-        <img className="logo-team-small" src={`/team-logos/${simData.teamData.abbreviation}.png`} alt="" />
+        <img
+          className="logo-team-small"
+          src={`/team-logos/${simData.teamData.abbreviation}.png`}
+          alt=""
+        />
       </div>
       <div className="simScore">
         {isHome ? simData.homeScoresAvg : simData.awayScoresAvg}
       </div>
       <ul>
         <li className="simInfoText">
-          Predicted score: {isHome ? simData.homeScoresAvg : simData.awayScoresAvg}
+          Predicted score:{" "}
+          {isHome ? simData.homeScoresAvg : simData.awayScoresAvg}
         </li>
         <li className="simInfoText">
           {`Has won ${simData.totalWinsPercentage}% of the games`}
         </li>
         <li className="simInfoText">
-          {`Has won ${isHome ? simData.totalHomeWinsPercentage : simData.totalAwayWinsPercentage}% of the ${isHome ? 'home' : 'away'} games`}
+          {`Has won ${
+            isHome
+              ? simData.totalHomeWinsPercentage
+              : simData.totalAwayWinsPercentage
+          }% of the ${isHome ? "home" : "away"} games`}
         </li>
         <li className="simInfoText">
           {`Simulation made of ${simData.totalGames} games (${simData.totalHomeGames} home, ${simData.totalAwayGames} away)`}
@@ -57,6 +84,7 @@ const Simulation = () => {
 
   return (
     <div className="simulation">
+      {renderTopBar()}
       <div className="simTeams">
         {renderTeamData(data[0], true)}
         {renderTeamData(data[1])}
