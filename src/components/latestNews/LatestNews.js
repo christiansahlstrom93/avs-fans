0e597c51ef736cf7c1168305d70e2c4e43871122
Christian Sahlström <christian.frahm.sahlstrom@ingka.ikea.com>
2022-06-09 15:58:55 +0200

    magi

--- src/components/latestNews/LatestNews.js
+++ src/components/latestNews/LatestNews.js
@@ -1,7 +1,53 @@
-import { useEffect, useContext } from "react";
+import { useEffect, useContext, useState } from "react";
 import { NewsContext } from "../../contexts/newsContext";
 
 import "./LatestNews.css";
+
+const Article = (props) => {
+  const [showMore, setShowMore] = useState(false);
+  const { article } = props;
+  const renderImg = (src, srcSet) => {
+    if (!srcSet && !src) return null;
+    return (
+      <div className="img-container">
+        <img className="article-img" src={src} srcSet={srcSet} />
+      </div>
+    );
+  };
+
+  const renderShowMore = () => {
+    if (!showMore) {
+      return (
+        <div className="show-more-container">
+          <button className="show-more" onClick={() => setShowMore(true)}>
+            Show more
+          </button>
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <div dangerouslySetInnerHTML={{ __html: article.body }} />
+        <div className="show-more-container">
+          <button onClick={() => setShowMore(false)} className="show-less">
+            Show less
+          </button>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <>
+      <h1>{article.headline}</h1>
+      <h4 className="subheader">{article.subHeader}</h4>
+      {renderImg(article.imgSrc, article.srcSet)}
+      <div dangerouslySetInnerHTML={{ __html: article.preview }} />
+      {renderShowMore()}
+    </>
+  );
+};
 
 const LatestNews = () => {
   const [{ data, loading }, fetchNews] = useContext(NewsContext);
@@ -23,24 +69,11 @@ const LatestNews = () => {
     return <div className="no-news">No news...</div>;
   }
 
-  const renderImg = (src) => {
-    if (!src) return null;
-    return (
-      <div className="img-container">
-        <img className="article-img" src={src} />
-      </div>
-    );
-  };
-
   const renderArticles = () => {
     return data.map((article, i) => {
       return (
         <div key={i} className="article">
-          <h3>{article.headline}</h3>
-          <h4>{article.subHeader}</h4>
-          {renderImg(article.imgSrc)}
-          <div dangerouslySetInnerHTML={{ __html: article.preview }} />
-          <div dangerouslySetInnerHTML={{ __html: article.body }} />
+          <Article article={article} />
         </div>
       );
     });
